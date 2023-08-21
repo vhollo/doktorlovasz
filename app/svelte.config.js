92ad78c240736512f5774d03f07c3749817a0b82
vhollo <vhollo@protonmail.com>
2023-08-21 15:41:49 +0200

    adapter-netlify

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,18 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import path from 'path'
+//import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-netlify'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+    postcss: true,
+  }),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+    adapter: adapter(),
+    alias: {
+      $components: path.resolve('./src/lib/components'),
+      $lib: path.resolve('./src/lib'),
+      $src: path.resolve('./src'),
+      $stores: path.resolve('./src/stores'),
+    },
 	}
-};
+}
 
 export default config;
