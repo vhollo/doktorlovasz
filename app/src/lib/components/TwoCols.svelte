d5e7909a9888b5cd8a500077a1c09e0a97266698
vhollo <vhollo@protonmail.com>
2023-10-31 17:20:08 +0100

    Details

---
+++ app/src/lib/components/TwoCols.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Comps from '$components/Comps.svelte';
+	import type { PageData } from '../../routes/$types';
+
+  export let content: PageData;
+	export let ix: Number;
+
+  console.log({content})
+</script>
+
+<article class="prose gap-x-8 gap-y-0 grid-cols-2xs">
+  {#if content.title}
+    {#if ix == 0}
+      <h1>{content.title}</h1>
+    {:else}
+      <h2>{content.title}</h2>
+    {/if}
+  {/if}
+
+  <section>
+    {#each content.col1.pageBuilder as col, ix}
+      <Comps content={col} {ix}/>
+    {/each}
+  </section>
+
+  <section>
+    {#each content.col2.pageBuilder as col, ix}
+      <Comps content={col} {ix}/>
+    {/each}
+  </section>
+</article>
